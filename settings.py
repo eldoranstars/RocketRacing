c12c768e4442b0667646084b53486adb825b3b6a
Igor.Dorozhkin <Igor.Dorozhkin@nedra.digital>
2024-02-08 10:49:11 +0300

    fixFireImage

--- settings.py
+++ settings.py
@@ -17,6 +17,17 @@ class Settings():
         self.car_red_color = (255, 0, 0)
         self.car_green_color = (0, 255, 0)
         # Параметры изображений
+        self.fire00 = pygame.image.load(resource_path('media/fire00.png'))
+        self.fire01 = pygame.image.load(resource_path('media/fire01.png'))
+        self.fire02 = pygame.image.load(resource_path('media/fire02.png'))
+        self.fire03 = pygame.image.load(resource_path('media/fire03.png'))
+        self.fire04 = pygame.image.load(resource_path('media/fire04.png'))
+        self.fire05 = pygame.image.load(resource_path('media/fire05.png'))
+        self.fire06 = pygame.image.load(resource_path('media/fire06.png'))
+        self.fire07 = pygame.image.load(resource_path('media/fire07.png'))
+        self.fire08 = pygame.image.load(resource_path('media/fire08.png'))
+        self.fire09 = pygame.image.load(resource_path('media/fire09.png'))
+        self.fire10 = pygame.image.load(resource_path('media/fire10.png'))
         self.fire11 = pygame.image.load(resource_path('media/fire11.png'))
         self.fire12 = pygame.image.load(resource_path('media/fire12.png'))
         self.fire13 = pygame.image.load(resource_path('media/fire13.png'))
@@ -25,6 +36,8 @@ class Settings():
         self.fire16 = pygame.image.load(resource_path('media/fire16.png'))
         self.fire17 = pygame.image.load(resource_path('media/fire17.png'))
         self.fire18 = pygame.image.load(resource_path('media/fire18.png'))
+        self.fire19 = pygame.image.load(resource_path('media/fire19.png'))
+        self.fire20 = pygame.image.load(resource_path('media/fire20.png'))
         self.fire21 = pygame.image.load(resource_path('media/fire21.png'))
         self.fire22 = pygame.image.load(resource_path('media/fire22.png'))
         self.fire23 = pygame.image.load(resource_path('media/fire23.png'))
@@ -33,6 +46,8 @@ class Settings():
         self.fire26 = pygame.image.load(resource_path('media/fire26.png'))
         self.fire27 = pygame.image.load(resource_path('media/fire27.png'))
         self.fire28 = pygame.image.load(resource_path('media/fire28.png'))
+        self.fire29 = pygame.image.load(resource_path('media/fire29.png'))
+        self.fire30 = pygame.image.load(resource_path('media/fire30.png'))
         self.fire31 = pygame.image.load(resource_path('media/fire31.png'))
         self.fire32 = pygame.image.load(resource_path('media/fire32.png'))
         self.fire33 = pygame.image.load(resource_path('media/fire33.png'))
@@ -41,6 +56,8 @@ class Settings():
         self.fire36 = pygame.image.load(resource_path('media/fire36.png'))
         self.fire37 = pygame.image.load(resource_path('media/fire37.png'))
         self.fire38 = pygame.image.load(resource_path('media/fire38.png'))
+        self.fire39 = pygame.image.load(resource_path('media/fire39.png'))
+        self.fire40 = pygame.image.load(resource_path('media/fire40.png'))
         self.fire41 = pygame.image.load(resource_path('media/fire41.png'))
         self.fire42 = pygame.image.load(resource_path('media/fire42.png'))
         self.fire43 = pygame.image.load(resource_path('media/fire43.png'))
@@ -48,29 +65,12 @@ class Settings():
         self.fire45 = pygame.image.load(resource_path('media/fire45.png'))
         self.fire46 = pygame.image.load(resource_path('media/fire46.png'))
         self.fire47 = pygame.image.load(resource_path('media/fire47.png'))
-        self.fire48 = pygame.image.load(resource_path('media/fire48.png'))
-        self.fire51 = pygame.image.load(resource_path('media/fire51.png'))
-        self.fire52 = pygame.image.load(resource_path('media/fire52.png'))
-        self.fire53 = pygame.image.load(resource_path('media/fire53.png'))
-        self.fire54 = pygame.image.load(resource_path('media/fire54.png'))
-        self.fire55 = pygame.image.load(resource_path('media/fire55.png'))
-        self.fire56 = pygame.image.load(resource_path('media/fire56.png'))
-        self.fire57 = pygame.image.load(resource_path('media/fire57.png'))
-        self.fire58 = pygame.image.load(resource_path('media/fire58.png'))
-        self.fire61 = pygame.image.load(resource_path('media/fire61.png'))
-        self.fire62 = pygame.image.load(resource_path('media/fire62.png'))
-        self.fire63 = pygame.image.load(resource_path('media/fire63.png'))
-        self.fire64 = pygame.image.load(resource_path('media/fire64.png'))
-        self.fire65 = pygame.image.load(resource_path('media/fire65.png'))
-        self.fire66 = pygame.image.load(resource_path('media/fire66.png'))
-        self.fire67 = pygame.image.load(resource_path('media/fire67.png'))
-        self.fire68 = pygame.image.load(resource_path('media/fire68.png'))
-        self.firelist = [self.fire11, self.fire12, self.fire13, self.fire14, self.fire15, self.fire16, self.fire17, self.fire18, \
-            self.fire21, self.fire22, self.fire23, self.fire24, self.fire25, self.fire26, self.fire27, self.fire28, \
-            self.fire31, self.fire32, self.fire33, self.fire34, self.fire35, self.fire36, self.fire37, self.fire38, \
-            self.fire41, self.fire42, self.fire43, self.fire44, self.fire45, self.fire46, self.fire47, self.fire48, \
-            self.fire51, self.fire52, self.fire53, self.fire54, self.fire55, self.fire56, self.fire57, self.fire58, \
-            self.fire61, self.fire62, self.fire63, self.fire64, self.fire65, self.fire66, self.fire67, self.fire68]
+        self.firelist = [self.fire00, self.fire01, self.fire02, self.fire03, self.fire04, self.fire05, self.fire06, self.fire07, \
+            self.fire08, self.fire09, self.fire10, self.fire11, self.fire12, self.fire13, self.fire14, self.fire15, \
+            self.fire16, self.fire17, self.fire18, self.fire19, self.fire20, self.fire21, self.fire22, self.fire23, \
+            self.fire24, self.fire25, self.fire26, self.fire27, self.fire28, self.fire29, self.fire30, self.fire31, \
+            self.fire32, self.fire33, self.fire34, self.fire35, self.fire36, self.fire37, self.fire38, self.fire39, \
+            self.fire40, self.fire41, self.fire42, self.fire43, self.fire44, self.fire45, self.fire46, self.fire47]
         self.road_surface = pygame.image.load(resource_path('media/road.jpg'))
         self.oil_surface = pygame.image.load(resource_path('media/oil.png'))
         self.oil_surface = pygame.transform.scale(self.oil_surface, (85,50))
@@ -125,4 +125,4 @@ class Settings():
         self.tractor_chance_increment = 0
         self.tractors_move_right = []
         self.tractors_move_left = []
-        self.crash_reload = 47
+        self.crash_reload = len(self.firelist)
